ba97cc881c96ab71559e94574604ebf86708f867
Tanya Grunina <tanya.grunina@gmail.com>
2025-05-13 21:59:17 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ async def friday_report(application):
     await asyncio.sleep(5)
     while True:
         now = now_moscow()
-        if now.weekday() == 1 and now.hour == 20 and now.minute == 55:
+        if now.weekday() == 1 and now.hour == 21 and now.minute == 2:
             logging.info("Настало время пятничного отчёта...")
             chat_ids = {c for (_, _, _, c) in payments}
             for chat_id in chat_ids:
